2947c7694fa2666d307833282c24a9f0136ff99e
apokaslan <100843601+apokaslan@users.noreply.github.com>
2022-09-08 16:11:42 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(12);
+        myList.add(24);
+        myList.add(36);
+        myList.add(81);
+        myList.add(52);
+        myList.add(64);
+        myList.add(75);
+        myList.add(82);
+        myList.add(96);
+        
+    }
+}
